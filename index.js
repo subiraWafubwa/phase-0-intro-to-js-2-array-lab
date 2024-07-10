8e8a8b860664e9324e81718b066440f4d08e0f57
Subira Wafubwa <subira.craig.2023@gmail.com>
2024-07-10 20:16:11 +0300

    Last Commit

--- index.js
+++ index.js
@@ -1,54 +1,34 @@
-// Initial cats array
+
 let cats = ["Milo", "Otis", "Garfield"];
 
-// Destructively appends a cat to the end of the cats array
 function destructivelyAppendCat(name) {
     cats.push(name);
 }
 
-// Destructively prepends a cat to the beginning of the cats array
 function destructivelyPrependCat(name) {
     cats.unshift(name);
 }
 
-// Destructively removes the last cat from the cats array
 function destructivelyRemoveLastCat() {
     cats.pop();
 }
 
-// Destructively removes the first cat from the cats array
 function destructivelyRemoveFirstCat() {
     cats.shift();
 }
 
-// Non-destructively appends a cat to the cats array and returns a new array
 function appendCat(name) {
     return [...cats, name];
 }
 
-// Non-destructively prepends a cat to the cats array and returns a new array
 function prependCat(name) {
     return [name, ...cats];
 }
 
-// Non-destructively removes the last cat from the cats array and returns a new array
 function removeLastCat() {
     return cats.slice(0, cats.length - 1);
 }
 
-// Non-destructively removes the first cat from the cats array and returns a new array
 function removeFirstCat() {
     return cats.slice(1);
 }
-
-module.exports = {
-    cats,
-    destructivelyAppendCat,
-    destructivelyPrependCat,
-    destructivelyRemoveLastCat,
-    destructivelyRemoveFirstCat,
-    appendCat,
-    prependCat,
-    removeLastCat,
-    removeFirstCat
-};
